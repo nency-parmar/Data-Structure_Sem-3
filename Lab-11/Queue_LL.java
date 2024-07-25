d940079456187ee223a4cec1630fac74f8221704
Nency Parmar <148302864+nencyp1604@users.noreply.github.com>
2024-07-25 18:44:17 +0530

    Add files via upload

---
+++ Lab-11/Queue_LL.java
@@ -0,0 +1,104 @@
+import java.util.*;
+class Stack 
+{
+    class Node 
+    {
+        int data;
+        Node link;
+        Node(int data) 
+        {
+            this.data = data;
+            this.link = null;
+        }
+    }
+    Node top = null;
+
+    public void Push(int data) 
+    {
+        Node new_node = new Node(data);
+        if (top == null) 
+        {
+            top = new_node;
+            return;
+        }
+        Node temp = top;
+        top = new_node;
+        top.link = temp;
+    }
+
+    public void Pop() 
+    {
+        Node save;
+        if (top == null) 
+        {
+            System.out.println("Stack Underflow");
+        } 
+        else 
+        {
+            Node Save = top;
+            while (Save.link.link != null) 
+            {
+                Save = Save.link;    
+            }
+            Save.link = null;
+        }
+    }
+
+    public void displaystack()
+    {
+        if(top == null)
+        {
+            System.out.println("Linked list is Empty.");
+        }
+        Node node = top;
+        while (node != null) 
+        {
+            System.out.print(node.data + "->");
+            node = node.link;
+        }
+        System.out.println("NULL");
+    }
+}
+public class Queue_LL
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        Stack s = new Stack();
+        s.Push(1);
+        s.displaystack();
+        while (true) 
+        {
+            System.out.println("Enter 1 to Push");
+            System.out.println("Enter 2 to Pop");
+            System.out.println("Enter 3 to Display Stack");
+            System.out.println("Enter 4 to Exit");
+            int a = sc.nextInt();
+            if (a == 1) 
+            {
+                System.out.println("Enter element you want to push in Stack:");
+                int b = sc.nextInt();
+                s.Push(b);
+                // s.displaystack();
+            }
+            else if (a == 2) 
+            {
+                s.Pop();
+                // s.displaystack();
+            }
+            else if (a == 3) 
+            {
+                s.displaystack();
+            }
+            else if (a == 4) 
+            {
+                System.out.println("Linked List is Closed.");
+                break;
+            }
+            else
+            {
+                System.out.println("Enter Valid Number.");
+            }
+        }
+    }
+}
